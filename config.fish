5beee9e03912573340c3fb0c01f7880b8924157a
Dmitriy Sorkin <aikixd@gmail.com>
2024-02-07 23:19:45 +0200

    .

---
+++ config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  alias nvimex="XDG_CONFIG_HOME=~/.config/nvim-ex/ XDG_DATA_HOME=~/.local/share/nvim-ex/ XDG_RUNTRIME_DIR=/tmp/nvim-ex.$USER/ XDG_STATE_DIR=~/.local/state/nvim-ex/ nvim"
+  alias config="/usr/bin/git -C $HOME/.config"
+
+  set -l ls_args "--color=always -lah"
+  alias ll="ls $ls_args | grep "^d" && ls $ls_args | grep \"^-\" && ls $ls_args | grep \"^l\""
+
+
+  set -g fish_ambiguous_width 2
+  set -g fish_emoji_width 2
+
+  set -g fish_key_bindings fish_vi_key_bindings
+  set -g fish_cursor_insert line
+
+  set -gx MANPAGER 'nvim +Man!'
+  set -gx MANWIDTH 999
+else
+  echo 'non-interactive'
+end
+
+set -gx CUDACXX /usr/local/cuda-12.3/bin/nvcc
